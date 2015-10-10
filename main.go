158be604380f655c8fc00b9323160b0a967f79f9
Craig Peterson <cpeterson@stackoverflow.com>
2015-10-10 13:46:11 -0600

    port

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
 	r.Run(":8765") // listen and serve on 0.0.0.0:8080
