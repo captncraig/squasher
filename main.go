948719dfb4e80f82ed0061dffe5676de0fb56c69
Craig Peterson <cpeterson@stackoverflow.com>
2015-10-10 13:57:20 -0600

    adding ghauth

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/captncraig/ghauth"
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func main() {
 	r := gin.Default()
+
+	// first create the auth handler
+	conf := &ghauth.Conf{
+		ClientId:     os.Getenv("GH_CLIENT_ID"),
+		ClientSecret: os.Getenv("GH_CLIENT_SECRET"),
+		Scopes:       []string{"user", "repo"},
+		CookieName:   "ghuser",
+		CookieSecret: os.Getenv("COOKIE_SECRET"),
+	}
+	ghauth.New(conf)
+
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "hello")
 	})
-	r.Run(":8765") // listen and serve on 0.0.0.0:8080
+	r.Run(":8765")
 }
