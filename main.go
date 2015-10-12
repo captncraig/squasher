49ac01a9e8b99084f48df0552e99cc2b4d741135
Craig Peterson <cpeterson@stackoverflow.com>
2015-10-12 07:43:33 -0600

    working on flow

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/captncraig/ghauth"
 	"github.com/captncraig/temple"
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/github"
 )
 
 //go:generate govendor add +external
@@ -40,9 +42,14 @@ func main() {
 	r.StaticFS("/static", FS(useDev))
 	r.GET("/", home)
 
+	locked := r.Group("/", auth.RequireAuth())
+	locked.GET("/repo/:owner/:repo", repo)
+	locked.GET("/repo/:owner/:repo/:pr", pull)
+
 	r.Run(":8765")
 }
 func render(c *gin.Context, name string, data interface{}) {
+	c.Header("Content-Type", "text/html")
 	if err := templateManager.Execute(c.Writer, data, name); err != nil {
 		c.AbortWithError(500, err)
 	}
@@ -56,9 +63,66 @@ func renderError(c *gin.Context) {
 		render(c, "error", gin.H{"User": u, "Errors": errs})
 	}
 }
+
+func getIntQuery(ctx *gin.Context, name string, def int) int {
+	q := ctx.Query(name)
+	i, err := strconv.Atoi(q)
+	if err != nil {
+		return def
+	}
+	return i
+}
+
 func home(ctx *gin.Context) {
 	u := ghauth.User(ctx)
-	ctx.Header("Content-Type", "text/html")
 	data := gin.H{"User": u}
+	if u != nil {
+		opts := &github.RepositoryListOptions{}
+		opts.PerPage = 20
+		opts.Page = getIntQuery(ctx, "page", 0)
+		opts.Sort = "pushed"
+		repos, res, err := u.Client().Repositories.List("", opts)
+		if err != nil {
+			ctx.Error(err)
+			return
+		}
+		data["Result"] = res
+		data["Repos"] = repos
+	}
 	render(ctx, "home", data)
+}
+
+func repo(ctx *gin.Context) {
+	u := ghauth.User(ctx)
+	owner, repo := ctx.Param("owner"), ctx.Param("repo")
+	data := gin.H{"User": u, "Owner": owner, "Repo": repo}
+	opts := &github.PullRequestListOptions{}
+	opts.PerPage = 20
+	opts.Page = getIntQuery(ctx, "page", 0)
+	pulls, res, err := u.Client().PullRequests.List(owner, repo, opts)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+	data["Pulls"] = pulls
+	data["Result"] = res
+	render(ctx, "repo", data)
+}
+
+func pull(ctx *gin.Context) {
+	owner, repo, num := ctx.Param("owner"), ctx.Param("repo"), ctx.Param("pr")
+	number, _ := strconv.Atoi(num)
+	u := ghauth.User(ctx)
+	data := gin.H{"User": u, "Owner": owner, "Repo": repo, "Number": number}
+
+	pr, res, err := u.Client().PullRequests.Get(owner, repo, number)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+	data["Pull"] = pr
+	data["Result"] = res
+	data["Merged"] = *pr.Merged
+	data["Conflicts"] = !*pr.Mergeable
+	render(ctx, "pr", data)
 }
